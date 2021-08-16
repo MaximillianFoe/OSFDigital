ab2f6dcb47e81f07ac503995975f489e8d3e7a1f
MaximillianFoe <cicoban@yandex.com>
2021-08-16 14:58:16 +0300

    Header.js created and added to Header. Also Divisions are merged for better experience. Cookie alert is also added. Next thing to do is align footer titles.

--- Footer.js
+++ Footer.js
@@ -1,3 +1,46 @@
 let FooterDate = new Date();
 let FullYear = FooterDate.getFullYear();
 document.getElementById("CurrentYear").innerHTML = FullYear;
+
+$(document).ready(function() {
+    let cookie = false;
+    let cookieContent = $('.CookieWarning');
+
+    checkCookie();
+
+    if (cookie === true) {
+        cookieContent.hide();
+    }
+
+    function setCookie(cname, cvalue, exdays) {
+        let d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        let expires = "expires=" + d.toGMTString();
+        document.cookie = cname + "=" + cvalue + "; " + expires;
+    }
+
+    function getCookie(cname) {
+        let name = cname + "=";
+        let ca = document.cookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i].trim();
+            if (c.indexOf(name) === 0) return c.substring(name.length, c.length);
+        }
+        return "";
+    }
+
+    function checkCookie() {
+        let check = getCookie("acookie");
+        if (check !== "") {
+            return cookie = true;
+        } else {
+            return cookie = false;
+        }
+
+    }
+
+    $('.CookieAccept').click(function () {
+        setCookie("acookie", "accepted", 365);
+        cookieContent.hide(500);
+    });
+});
