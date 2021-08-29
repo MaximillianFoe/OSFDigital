9345b298363cd76e84ca3ab82898893f478f0a62
MaximillianFoe <cicoban@yandex.com>
2021-08-29 17:52:16 +0300

    Login modal is finished with HTML5 validations! Toggle also added!

--- Header.js
+++ Header.js
@@ -1,1 +1,8 @@
-
+function showPassword() {
+    var x = document.getElementById("passwordForm");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
