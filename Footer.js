6c2e20f096fdb2b1473eb49fbe764b9ce8a467ef
MaximillianFoe <cicoban@yandex.com>
2021-08-29 16:45:28 +0300

    All footer is revamped. JS and CSS is fixed. Also cookie alert is implemented with Local Storage and working well!

--- Footer.js
+++ Footer.js
@@ -2,45 +2,22 @@ let FooterDate = new Date();
 let FullYear = FooterDate.getFullYear();
 document.getElementById("CurrentYear").innerHTML = FullYear;
 
-$(document).ready(function() {
-    let cookie = false;
-    let cookieContent = $('.CookieWarning');
+if (!localStorage.bannerClosed) {
+    $('.CookieAlert').css('display', 'inherit');
+} else {
+    $('.CookieAlert').css('display', 'none');
+}
 
-    checkCookie();
-
-    if (cookie === true) {
-        cookieContent.hide();
-    }
-
-    function setCookie(cname, cvalue, exdays) {
-        let d = new Date();
-        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-        let expires = "expires=" + d.toGMTString();
-        document.cookie = cname + "=" + cvalue + "; " + expires;
-    }
-
-    function getCookie(cname) {
-        let name = cname + "=";
-        let ca = document.cookie.split(';');
-        for (let i = 0; i < ca.length; i++) {
-            let c = ca[i].trim();
-            if (c.indexOf(name) === 0) return c.substring(name.length, c.length);
-        }
-        return "";
-    }
-
-    function checkCookie() {
-        let check = getCookie("acookie");
-        if (check !== "") {
-            return cookie = true;
-        } else {
-            return cookie = false;
-        }
-
-    }
-
-    $('.CookieAccept').click(function () {
-        setCookie("acookie", "accepted", 365);
-        cookieContent.hide(500);
-    });
+$('.CookieAlert button').click(function() {
+    $('.CookieAlert').css('display', 'none');
+    localStorage.bannerClosed = 'true';
 });
+
+$('.banner-accept').click(function() {
+    $('.CookieAlert').css('display', 'none');
+    localStorage.bannerClosed = 'true';
+});
+
+if (navigator.userAgent.match(/Opera|OPR\//)) {
+    $('.CookieAlert').css('display', 'inherit');
+}
