51112ca854b0a5ed8f19b07dbf91a0d24b93749e
MaximillianFoe <cicoban@yandex.com>
2021-08-31 05:59:00 +0300

    Final version with little bugs. Going to share on my personal URL.

--- Header.js
+++ Header.js
@@ -1,8 +1,27 @@
 function showPassword() {
-    var x = document.getElementById("passwordForm");
+    let x = document.getElementById("passwordForm");
     if (x.type === "password") {
         x.type = "text";
     } else {
         x.type = "password";
     }
 }
+
+let FavoriteItemCount = localStorage.getItem('FavoriteItemCount');
+let CartItemCount = localStorage.getItem('CartItemCount');
+
+if (FavoriteItemCount == null) {
+    FavoriteItemCount = 0;
+}
+
+if (CartItemCount == null) {
+    CartItemCount = 0;
+}
+
+function AddToFavorite() {
+    localStorage.setItem('FavoriteItemCount', FavoriteItemCount++);
+}
+function AddToCart() {
+    localStorage.setItem('CartItemCount', CartItemCount++);
+}
+
--- Index.js
+++ Index.js
@@ -1,18 +1,2 @@
-let FavoriteItemCount = localStorage.getItem('FavoriteItemCount');
-let CartItemCount = localStorage.getItem('CartItemCount');
 
-if (FavoriteItemCount == null) {
-    FavoriteItemCount = 0;
-}
-
-if (CartItemCount == null) {
-    CartItemCount = 0;
-}
-
-function AddToFavorite() {
-    localStorage.setItem('FavoriteItemCount', FavoriteItemCount++);
-}
-function AddToCart() {
-    localStorage.setItem('CartItemCount', CartItemCount++);
-}
 
